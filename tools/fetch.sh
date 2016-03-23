6572244843622b83efc69f15b157c88055d45bed
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2016-03-22 23:05:37 -0400

    Bumped RapidJSON version for new trailing commas

--- tools/fetch.sh
+++ tools/fetch.sh
@@ -10,11 +10,9 @@ MPACK_URL="https://github.com/ludocode/mpack/releases/download/v${MPACK_VERSION}
 curl -L -o "${MPACK_FILE}" "${MPACK_URL}" || exit $?
 
 # fetch rapidjson
-# (temporarily using our fork of rapidjson for trailing comma support)
-# https://github.com/ludocode/rapidjson
-RAPIDJSON_COMMIT="9fce30c06648519f3d44a2599ae91015f4dc311c"
+RAPIDJSON_COMMIT="ec322005072076ef53984462fb4a1075c27c7dfd"
 RAPIDJSON_FILE="rapidjson-${RAPIDJSON_COMMIT}.tar.gz"
-RAPIDJSON_URL="https://github.com/ludocode/rapidjson/archive/${RAPIDJSON_COMMIT}.tar.gz"
+RAPIDJSON_URL="https://github.com/miloyip/rapidjson/archive/${RAPIDJSON_COMMIT}.tar.gz"
 curl -L -o "${RAPIDJSON_FILE}" "${RAPIDJSON_URL}" || exit $?
 
 # fetch yajl
