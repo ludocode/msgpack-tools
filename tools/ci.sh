47df8c8bc3fd45e5a6e41463bb3565f207946865
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2016-07-16 19:50:09 -0400

    Updated CI to make Debian package

--- tools/ci.sh
+++ tools/ci.sh
@@ -8,18 +8,26 @@ else
     CMAKE_BUILD_TYPE=Release
 fi
 
+export CTEST_OUTPUT_ON_FAILURE=1
+
 mkdir -p build
 cd build
 
 if [[ "$CC" == "scan-build" ]]; then
     unset CXX
     CC=`which clang`
-    cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX="./install" .. || exit $?
+    cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX="./prefix" .. || exit $?
     unset CC
-    scan-build -o analysis --use-cc=`which clang` --status-bugs make all test install || exit $?
+    scan-build -o analysis --use-cc=`which clang` --status-bugs make all test || exit $?
 else
-    cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX="./install" .. || exit $?
-    make all test install || exit $?
+    cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX="./prefix" .. || exit $?
+    make all test || exit $?
 fi
 
-find install
+if [ -e /etc/debian_version ]; then
+    make package
+fi
+
+make install
+echo "  Installed files:"
+find prefix
--- tools/debug.sh
+++ tools/debug.sh
@@ -2,6 +2,6 @@
 BUILD="`dirname $0`/../build"
 mkdir -p "$BUILD"
 cd "$BUILD"
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="./install" .. || exit $?
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="./prefix" .. || exit $?
 make || exit $?
 CTEST_OUTPUT_ON_FAILURE=1 make test || exit $?
