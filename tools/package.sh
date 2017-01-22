904e6700ba77ca43c2b8ec617b5306b92166e876
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2017-01-22 14:58:30 -0500

    Updated packaging script

--- tools/package.sh
+++ tools/package.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+tools/debug.sh || exit $?
+
 MAJOR=`cat CMakeLists.txt | grep "set(PROJECT_VERSION_MAJOR" | grep -o '[0-9][0-9]*'`
 MINOR=`cat CMakeLists.txt | grep "set(PROJECT_VERSION_MINOR" | grep -o '[0-9][0-9]*'`
 PATCH=`cat CMakeLists.txt | grep "set(PROJECT_VERSION_PATCH" | grep -o '[0-9][0-9]*'`
@@ -25,29 +27,9 @@ FILES="\
     docs/json2msgpack.1 \
     docs/msgpack2json.1 \
     src \
-    src/common.h \
-    src/json2msgpack.cpp \
-    src/msgpack2json.cpp \
+    src/* \
     tests \
-    tests/base64-bin-ext.mp \
-    tests/base64-bin.mp \
-    tests/base64-debug.json \
-    tests/base64-detect.json \
-    tests/base64-mixed.json \
-    tests/base64-partial-ext.mp \
-    tests/base64-partial.mp \
-    tests/base64-prefix-lax.json \
-    tests/base64-prefix.json \
-    tests/base64-str-prefix.mp \
-    tests/base64-str.mp \
-    tests/basic-lax.json \
-    tests/basic-min.json \
-    tests/basic.json \
-    tests/basic.mp \
-    tests/value-int.json \
-    tests/value-int.mp \
-    tests/value-string.json \
-    tests/value-string.mp \
+    tests/* \
     tools \
     tools/debug.sh \
     tools/valgrind.sh \
@@ -60,3 +42,5 @@ cat docs/json2msgpack.md | sed "s/\$version/$VERSION/" | md2man-roff > docs/json
 cat docs/msgpack2json.md | sed "s/\$version/$VERSION/" | md2man-roff > docs/msgpack2json.1 || exit $?
 
 tar -czf ${NAME}.tar.gz --transform "s@^@$NAME/@" --no-recursion ${FILES}
+
+echo "Wrote ${NAME}.tar.gz"
